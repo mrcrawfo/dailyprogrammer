2de9bfe3d62314ef6458e0672ad8a4188e7b392f
Max Crawford <mrcrawfo@gmail.com>
2017-10-04 09:31:08 -0400

    Basic HTML/JS/CSS template folder to copy for future projects

---
+++ template/main.js
@@ -0,0 +1,12 @@
+var challenge1 = 1;
+var challenge2 = 2;
+var challenge3 = 3;
+var challenge4 = 4;
+var challenge5 = 5;
+var challenge6 = 6;
+var challenge7 = 7;
+var challenge8 = 8;
+
+function solve(input) {
+    // do something
+}
