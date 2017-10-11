156690b5d1ba71efd0e5a16eb93c4c1e31eda8c2
Max Crawford <mrcrawfo@gmail.com>
2017-10-11 10:57:31 -0400

    [108I] Replaced starburst symbol, began expanding from generator into playable game

--- dpc_108i/main.js
+++ dpc_108i/main.js
@@ -3,6 +3,14 @@ var challenge2 = [8, 8, 8];
 var challenge3 = [12, 12, 140];
 var challenge4 = [20, 20, 100];
 
+function generateGameData(data, row, col) {
+    var gameData = generate(data);
+    while(gameData[row].charAt(col) != 0) {
+        gameData = generate(data);
+    }
+    return gameData;
+}
+
 function generate(data) {
     var rows = data[0];
     var cols = data[1];
@@ -23,7 +31,7 @@ function generate(data) {
             r = Math.floor(Math.random() * rows);
             c = Math.floor(Math.random() * cols);
         }
-        cd[r] = cd[r].substr(0, c) + '★' + cd[r].substr(c + 1);
+        cd[r] = cd[r].substr(0, c) + '✷' + cd[r].substr(c + 1);
     }
     
     for(var r2 = 0; r2 < rows; r2++) {
@@ -35,7 +43,7 @@ function generate(data) {
             indexDown = -1;
         }
         for(var c2 = 0; c2 < cols; c2++) {
-            if (cd[r2].charAt(c2) != '★') {
+            if (cd[r2].charAt(c2) != '✷') {
                 var indexRight = c2 + 1;
                 var indexLeft = c2 - 1;
                 if (c2 == 0) {
@@ -48,14 +56,14 @@ function generate(data) {
                 // n4 rc n5
                 // n6 n7 n8
 
-                var m1 = (indexUp >= 0 && indexLeft >= 0 && cd[indexUp].split('')[indexLeft] == '★') ? true : false;
-                var m2 = (indexUp >= 0 && cd[indexUp].split('')[c2] == '★') ? true : false;
-                var m3 = (indexUp >= 0 && indexRight >= 0 && cd[indexUp].split('')[indexRight] == '★') ? true : false;
-                var m4 = (indexLeft >= 0 && cd[r2].split('')[indexLeft] == '★') ? true : false;
-                var m5 = (indexRight >= 0 && cd[r2].split('')[indexRight] == '★') ? true : false;
-                var m6 = (indexDown >= 0 && indexLeft >= 0  && cd[indexDown].split('')[indexLeft] == '★') ? true : false;
-                var m7 = (indexDown >= 0 && cd[indexDown].split('')[c2] == '★') ? true : false;
-                var m8 = (indexDown >= 0 && indexRight >= 0 && cd[indexDown].split('')[indexRight] == '★') ? true : false;
+                var m1 = (indexUp >= 0 && indexLeft >= 0 && cd[indexUp].split('')[indexLeft] == '✷') ? true : false;
+                var m2 = (indexUp >= 0 && cd[indexUp].split('')[c2] == '✷') ? true : false;
+                var m3 = (indexUp >= 0 && indexRight >= 0 && cd[indexUp].split('')[indexRight] == '✷') ? true : false;
+                var m4 = (indexLeft >= 0 && cd[r2].split('')[indexLeft] == '✷') ? true : false;
+                var m5 = (indexRight >= 0 && cd[r2].split('')[indexRight] == '✷') ? true : false;
+                var m6 = (indexDown >= 0 && indexLeft >= 0  && cd[indexDown].split('')[indexLeft] == '✷') ? true : false;
+                var m7 = (indexDown >= 0 && cd[indexDown].split('')[c2] == '✷') ? true : false;
+                var m8 = (indexDown >= 0 && indexRight >= 0 && cd[indexDown].split('')[indexRight] == '✷') ? true : false;
 
                 var neighbors = m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8;
 
