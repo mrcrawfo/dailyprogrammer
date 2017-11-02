7670a840dd0fae278bc18525320f2fd2522c7a56
Max Crawford <mrcrawfo@gmail.com>
2017-11-02 12:09:20 -0400

    [171E] Refactored to use hex arrays instead of string data, replaced switch statement with binary conversion, and added canvas bitmap rendering

--- dpc_171e/main.js
+++ dpc_171e/main.js
@@ -1,72 +1,59 @@
-var challenge1 = '183C7E7E18181818';
-var challenge2 = 'FF81BDA5A5BD81FF';
-var challenge3 = 'AA55AA55AA55AA55';
-var challenge4 = '3E7FFCF8F8FC7F3E';
-var challenge5 = '939393F3F3939393';
+var challenge1 = [0x18, 0x3C, 0x7E, 0x7E, 0x18, 0x18, 0x18, 0x18];
+var challenge2 = [0xFF, 0x81, 0xBD, 0xA5, 0xA5, 0xBD, 0x81, 0xFF];
+var challenge3 = [0xAA, 0x55, 0xAA, 0x55, 0xAA, 0x55, 0xAA, 0x55];
+var challenge4 = [0x3E, 0x7F, 0xFC, 0xF8, 0xF8, 0xFC, 0x7F, 0x3E];
+var challenge5 = [0x93, 0x93, 0x93, 0xF3, 0xF3, 0x93, 0x93, 0x93];
 
-function hexToBitmap(input) {
-    var output = '';
-    switch (input) {
-        case '0':
-            output = '    ';
-            break;
-        case '1':
-            output = '   X';
-            break;
-        case '2':
-            output = '  X ';
-            break;
-        case '3':
-            output = '  XX';
-            break;
-        case '4':
-            output = ' X  ';
-            break;
-        case '5':
-            output = ' X X';
-            break;
-        case '6':
-            output = ' XX ';
-            break;
-        case '7':
-            output = ' XXX';
-            break;
-        case '8':
-            output = 'X   ';
-            break;
-        case '9':
-            output = 'X  X';
-            break;
-        case 'A':
-            output = 'X X ';
-            break;
-        case 'B':
-            output = 'X XX';
-            break;
-        case 'C':
-            output = 'XX  ';
-            break;
-        case 'D':
-            output = 'XX X';
-            break;
-        case 'E':
-            output = 'XXX ';
-            break;
-        case 'F':
-            output = 'XXXX';
-            break;
+var canvasWidth = 8;
+var canvasHeight = 8;
+
+function hexToBool(input) {
+    var output = input.toString(2);
+    output = "00000000".substr(output.length) + output;
+    output = output.split('');
+    for(var i = 0; i < output.length; i++) {
+        output[i] = parseInt(output[i]) ? true : false;
     }
     return output;
 }
 
-function render(input) {
+function renderString(input) {
     var output = '';
-    input = input.split('');
     for(var i = 0; i < input.length; i++) {
-        output = output + hexToBitmap(input[i]);
-        if (i % 2 == 1) {
-            output += '\n';
+        var bools = hexToBool(input[i]);
+        for(var j = 0; j < bools.length; j++) {
+            if (bools[j]) {
+                output = output + 'X';
+            } else {
+                output = output + ' ';
+            }
         }
+        output += '\n';
     }
     return output;
+}
+
+function renderCanvas(canvas, input) {
+    var ctx = canvas.getContext('2d');
+    var imagedata = ctx.createImageData(canvasWidth, canvasHeight);
+    var data = imagedata.data;
+    var offset = 0;
+    for(var i = 0; i < input.length; i++) {
+        var bools = hexToBool(input[i]);
+        for(var j = 0; j < bools.length; j++) {
+            if (bools[j]) {
+                data[offset] = 0;
+                data[offset + 1] = 0;
+                data[offset + 2] = 0;
+                data[offset + 3] = 255;
+            } else {
+                data[offset] = 255;
+                data[offset + 1] = 255;
+                data[offset + 2] = 255;
+                data[offset + 3] = 255;
+            }
+            offset += 4;
+        }
+    }
+    ctx.putImageData(imagedata, 0, 0);
 }
